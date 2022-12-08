7b6b3c03be003cb96c8fe8bbd2082eff38907dad
Sean202222 <seanjohn.mc20@gmail.com>
2022-12-08 16:00:27 +0000

    api working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    response = HTTParty.get("https://api.publicapis.org/categories")
+    @response = JSON.parse(response.body)
+    @categories = @response["categories"]
   end
 end

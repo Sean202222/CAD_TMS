c0703a38dbd2d9bd53acb905a519c7b2ce35064e
Sean202222 <seanjohn.mc20@gmail.com>
2022-12-16 14:48:02 +0000

    UK Holiday Calendar API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  def home
-    response = HTTParty.get("https://api.publicapis.org/categories")
+  def home # Taken 'categories' from the API
+    # response = HTTParty.get("https://api.publicapis.org/categories")
+    response = HTTParty.get("https://www.gov.uk/bank-holidays.json")
     @response = JSON.parse(response.body)
-    @categories = @response["categories"]
+    @categories = @response["england-and-wales"]
+
+    @cat_facts = JSON.parse(HTTParty.get("https://cataas.com/api/tags").body)
   end
 end

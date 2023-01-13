6d076a36d6133dcf689ba69c68ce14e1dda77bdd
Sean202222 <seanjohn.mc20@gmail.com>
2023-01-13 20:09:49 +0000

    Cookies Set

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,25 @@ class PagesController < ApplicationController
     @categories = @response["england-and-wales"]
 
     @cat_facts = JSON.parse(HTTParty.get("https://cataas.com/api/tags").body)
+
+    # Set Cookies
+  def set_cookie
+    cookies.signed[:user_name] = "sean"
+    # Set Expiry Dates
+    cookies.signed[:user_number] = { value: "cookies YUM", expires: Time.now + 3600}
+  end
+  
+    # Show Cookies
+  def show_cookie
+    @user_name = cookies[:user_name]
+    @user_number = cookies[:user_number]
+  end
+
+  def delete_cookie
+    # Delete Cookies
+    cookies.delete :user_name
+    cookies.delete :user_number
+  end
+
   end
 end
